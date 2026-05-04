deb961cf09636fedea905f58c1454bba34a2515e
Loidy <loidychaka@gmail.com>
2026-05-04 23:05:19 +0200

    Conflict branch change

--- src/gitassignmentproject/GitAssignmentProject.java
+++ src/gitassignmentproject/GitAssignmentProject.java
@@ -14,7 +14,7 @@ public class GitAssignmentProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Version from master");
+        System.out.println("Version from conflict branch");
     }
     
 }
