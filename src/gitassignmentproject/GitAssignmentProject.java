01b1230bdad4523e53e13886e5b7a10b03a6435e
Loidy <loidychaka@gmail.com>
2026-05-04 22:44:40 +0200

    Updated Main.java in conflict-demo branch

--- src/gitassignmentproject/GitAssignmentProject.java
+++ src/gitassignmentproject/GitAssignmentProject.java
@@ -14,7 +14,7 @@ public class GitAssignmentProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Main branch version");
+        System.out.println("Conflict branch version");
     }
     
 }
